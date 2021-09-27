4c51a58a0d9c9b5ec3d71f658436298aad8fa2c3
Anthony Chen <anthonychen33@gmail.com>
2021-09-27 00:06:11 -0400

    added a clear button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const gridContainer = document.querySelector(".grid-container");
 
-//let counter = 0; 
-
+let counter = 0; 
+/* Generation of 16x16 grid STARTS*/
 for(let i = 0; i < 16; i++) {
     for(let j = 0; j < 16; j++) {
         
@@ -9,16 +9,17 @@ for(let i = 0; i < 16; i++) {
         squareElement.style = "background-color: #e6ebe6; height: 25px; width: 25px;"; 
         squareElement.classList.add("square");
         gridContainer.appendChild(squareElement);  
-    
-        if ( i % 4 === 0 ) {
-          //  squareElement.style = "background-color: blue; height: 25px; width: 25px;";
-        }
-       // counter++; 
-    }
 
+        counter++; 
+    
+    }
+    // Subtract one from height and width for anything > 16
 }
 
+console.log(counter); 
+/* Generation of 16x16 grid ENDS*/
 
+/* The "pen-like" effect of hovering over elements STARTS */
 const squareGrid = document.querySelectorAll(".square"); 
 
 squareGrid.forEach((squareBox) => {
@@ -29,5 +30,27 @@ squareGrid.forEach((squareBox) => {
        
     });
 });
-console.log(squareGrid); 
+/* The "pen-like" effect of hovering over elements ENDS */
+
+const clear = document.querySelector("#clearButton"); 
+let newGrid = null; 
+
+clear.addEventListener("click", (e) => {
+
+    squareGrid.forEach((squareBox) => {
+
+            squareBox.style.backgroundColor = "#e6ebe6";
+    });
+
+    newGrid = Number(prompt("Enter a number for a new grid"));
+
+    
+});
+
+
+
+
+
+
+//console.log(squareGrid); 
 //console.log(counter); 
